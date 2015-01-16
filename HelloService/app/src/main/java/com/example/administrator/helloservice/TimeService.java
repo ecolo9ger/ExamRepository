87e1aad0c59e2101269b16d1aa83214d18f04ba5
박영일 <ecologer@naver.com>
2015-01-17 01:06:57 +0900

    타이머 클래스 생성

--- HelloService/app/src/main/java/com/example/administrator/helloservice/TimeService.java
+++ HelloService/app/src/main/java/com/example/administrator/helloservice/TimeService.java
@@ -8,6 +8,13 @@ import android.os.IBinder;
  * Created by Administrator on 2015-01-17.
  */
 public class TimeService extends Service {
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        
+        return super.onStartCommand(intent, flags, startId);
+    }
+
     @Override
     public IBinder onBind(Intent intent) {
         return null;
